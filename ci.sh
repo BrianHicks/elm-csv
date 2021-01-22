6b144445f1fb7bd500e76266eee618ea907a726a
Brian Hicks <brian@brianthicks.com>
2021-01-22 09:18:52 -0600

    disable elm-review in CI (:cry:)

--- ci.sh
+++ ci.sh
@@ -10,5 +10,8 @@ elm-test
 elm make --docs=documentation.json
 
 # linting
-elm-review
 elm-format --validate src
+
+# elm-review tries to download elm-json, and it fails in CI. We'll try again
+# in the 20.05 release of Nix, where it's packaged natively.
+# elm-review
