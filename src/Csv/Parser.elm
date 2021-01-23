310411d9d3028c73ea4b67a65409d70085468112
Brian Hicks <brian@brianthicks.com>
2021-01-23 14:36:42 -0600

    change up the types

--- src/Csv/Parser.elm
+++ src/Csv/Parser.elm
@@ -27,10 +27,10 @@ type Config
 
 
 type alias InternalConfig =
-    { rowSeparator : String
-    , newRowIndicator : Char
-    , fieldSeparator : String
-    , newFieldIndicator : Char
+    { rowFirst : Char
+    , rowRest : String
+    , fieldFirst : Char
+    , fieldRest : String
     }
 
 
@@ -51,12 +51,12 @@ config :
     -> Result ConfigProblem Config
 config separators =
     case ( String.uncons separators.rowSeparator, String.uncons separators.fieldSeparator ) of
-        ( Just ( newRowIndicator, _ ), Just ( newFieldIndicator, _ ) ) ->
+        ( Just ( rowFirst, rowRest ), Just ( fieldFirst, fieldRest ) ) ->
             (Ok << Config)
-                { rowSeparator = separators.rowSeparator
-                , newRowIndicator = newRowIndicator
-                , fieldSeparator = separators.fieldSeparator
-                , newFieldIndicator = newFieldIndicator
+                { rowFirst = rowFirst
+                , rowRest = rowRest
+                , fieldFirst = fieldFirst
+                , fieldRest = fieldRest
                 }
 
         ( Nothing, _ ) ->
@@ -71,114 +71,15 @@ using `Csv.Decode.decodeCsv` or `Csv.Decode.decodeCustom`
 unless you need something unusally custom (and please [open an
 issue](https://github.com/BrianHicks/elm-csv/issues/new) if you do!)
 -}
-parse : Config -> String -> Result (List Parser.DeadEnd) (List (List String))
+parse : Config -> String -> Result String (List (List String))
 parse (Config internalConfig) source =
+    let
+        parseHelp : String -> List String -> List (List String) -> Int -> Int -> Result String (List (List String))
+        parseHelp nextSource row rows startOffset endOffset =
+            Err "NO"
+    in
     if String.isEmpty source then
         Ok []
 
     else
-        Parser.run (parser internalConfig) source
-
-
-parser : InternalConfig -> Parser (List (List String))
-parser config_ =
-    Parser.loop [] <|
-        \soFar ->
-            Parser.oneOf
-                [ Parser.succeed (\_ -> Parser.Done (List.reverse soFar))
-                    |= Parser.end
-                , Parser.succeed (\row -> Parser.Loop (row :: soFar))
-                    |= rowParser config_
-                ]
-
-
-rowParser : InternalConfig -> Parser (List String)
-rowParser config_ =
-    Parser.loop [] <|
-        \fields ->
-            Parser.map
-                (\( field, afterField ) ->
-                    case afterField of
-                        AnotherField ->
-                            Parser.Loop (field :: fields)
-
-                        NoMore ->
-                            Parser.Done (List.reverse (field :: fields))
-                )
-                (fieldParser config_)
-
-
-type AfterField
-    = AnotherField
-    | NoMore
-
-
-afterFieldParser : InternalConfig -> Parser AfterField
-afterFieldParser config_ =
-    Parser.oneOf
-        [ Parser.map (\_ -> AnotherField) (Parser.token config_.fieldSeparator)
-        , Parser.map (\_ -> NoMore) (Parser.token config_.rowSeparator)
-        , Parser.map (\_ -> NoMore) Parser.end
-        ]
-
-
-fieldParser : InternalConfig -> Parser ( String, AfterField )
-fieldParser config_ =
-    Parser.oneOf
-        [ quotedFieldParser config_
-        , unquotedFieldParser config_
-        ]
-
-
-quotedFieldParser : InternalConfig -> Parser ( String, AfterField )
-quotedFieldParser config_ =
-    Parser.succeed Tuple.pair
-        |. Parser.token quote
-        |= Parser.loop []
-            (\soFar ->
-                Parser.oneOf
-                    [ Parser.succeed (\() -> Parser.Loop ("\"" :: soFar))
-                        |= Parser.token escapedQuote
-                    , Parser.succeed (\() -> Parser.Done (String.concat (List.reverse soFar)))
-                        |= Parser.token quote
-                    , Parser.succeed (\segment -> Parser.Loop (segment :: soFar))
-                        |= Parser.getChompedString (Parser.chompUntil quote)
-                    ]
-            )
-        |= afterFieldParser config_
-
-
-unquotedFieldParser : InternalConfig -> Parser ( String, AfterField )
-unquotedFieldParser config_ =
-    Parser.loop [] <|
-        \segments ->
-            Parser.oneOf
-                [ Parser.succeed
-                    (\afterField ->
-                        Parser.Done
-                            ( String.concat (List.reverse segments)
-                            , afterField
-                            )
-                    )
-                    |= afterFieldParser config_
-                , Parser.succeed (\segment -> Parser.Loop (segment :: segments))
-                    |= Parser.oneOf
-                        [ Parser.chompIf (\c -> c == config_.newRowIndicator)
-                            |> Parser.getChompedString
-                        , Parser.chompIf (\c -> c == config_.newFieldIndicator)
-                            |> Parser.getChompedString
-                        , Parser.chompWhile
-                            (\c -> c /= config_.newFieldIndicator && c /= config_.newRowIndicator)
-                            |> Parser.getChompedString
-                        ]
-                ]
-
-
-quote : String
-quote =
-    "\""
-
-
-escapedQuote : String
-escapedQuote =
-    "\"\""
+        parseHelp source [] [] 0 0
