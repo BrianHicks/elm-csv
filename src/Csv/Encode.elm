718718a0002145c1735400e12b05cbb47bd1c8c8
Brian Hicks <brian@brianthicks.com>
2021-02-01 17:29:41 -0600

    document the API

--- src/Csv/Encode.elm
+++ src/Csv/Encode.elm
@@ -1,7 +1,6 @@
 module Csv.Encode exposing (encode, Encoder, withFieldNames, withoutFieldNames)
 
-{-| Turn a list of values into a CSV-formatted string, escaping characters
-as necessary.
+{-|
 
 @docs encode, Encoder, withFieldNames, withoutFieldNames
 
@@ -10,21 +9,66 @@ as necessary.
 import Dict exposing (Dict)
 
 
+{-| -}
 type Encoder a
     = WithFieldNames (List String) (a -> List ( String, String ))
     | WithoutFieldNames (a -> List String)
 
 
+{-| Encode your data with a header full of field names at the top.
+
+    [ ( "FF", "FF", "FF" )
+    , ( "80", "80", "80" )
+    , ( "00", "00", "00" )
+    ]
+        |> encode
+            { encoder =
+                withFieldNames
+                    [ "red", "green", "blue" ]
+                    (\( r, g, b ) ->
+                        [ ( "red", r )
+                        , ( "green", g )
+                        , ( "blue", b )
+                        ]
+                    )
+            , fieldSeparator = ','
+            }
+        --> "red,green,blue\r\nFF,FF,FF\r\n80,80,80\r\n00,00,00"
+
+If you provide a field name which isn't in the list for a row, it will be
+replaced with a blank field to avoid generating a misaligned CSV.
+
+-}
 withFieldNames : List String -> (a -> List ( String, String )) -> Encoder a
 withFieldNames =
     WithFieldNames
 
 
+{-| Encode your data whatver way you like. This is the "live an exciting
+adventure" encoder: it will let you output rows with uneven lengths. It will
+still escape quotes properly, however!
+
+    [ ( "FF", "FF", "FF" )
+    , ( "80", "80", "80" )
+    , ( "00", "00", "00" )
+    ]
+        |> encode
+            { encoder = withoutFieldNames (\(r,g, b) -> [ r, g, b ] )
+            , fieldSeparator = ','
+            }
+        --> "FF,FF,FF\r\n80,80,80\r\n00,00,00"
+
+-}
 withoutFieldNames : (a -> List String) -> Encoder a
 withoutFieldNames =
     WithoutFieldNames
 
 
+{-| Encode some data to a CSV string, quoting and escaping
+characters as necessary. See [`withFieldNames`](#withFieldNames) and
+[`withoutFieldNames`](#withoutFieldNames) for more on how to encode different
+kinds of data.
+-}
 encode :
     { encoder : Encoder a
     , fieldSeparator : Char
