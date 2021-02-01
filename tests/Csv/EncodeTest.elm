c99ea7a4c3f0f690f79e2dfbc8f8a97191af38f0
Brian Hicks <brian@brianthicks.com>
2021-02-01 16:29:43 -0600

    test that we use the right encoder

--- tests/Csv/EncodeTest.elm
+++ tests/Csv/EncodeTest.elm
@@ -7,13 +7,17 @@ import Test exposing (..)
 
 encodeTest : Test
 encodeTest =
+    let
+        pets =
+            [ { id = 1, name = "Atlas", species = "cat" }
+            , { id = 2, name = "Axel", species = "puffin" }
+            ]
+    in
     describe "encode"
         [ describe "with field names"
             [ test "encodes with field names" <|
                 \_ ->
-                    [ { id = 1, name = "Atlas", species = "cat" }
-                    , { id = 2, name = "Axel", species = "puffin" }
-                    ]
+                    pets
                         |> Encode.encode
                             { encoder =
                                 Encode.withFieldNames
@@ -29,9 +33,7 @@ encodeTest =
                         |> Expect.equal "ID,Name,Species\u{000D}\n1,Atlas,cat\u{000D}\n2,Axel,puffin"
             , test "encodes with field names even when the ordering does not match" <|
                 \_ ->
-                    [ { id = 1, name = "Atlas", species = "cat" }
-                    , { id = 2, name = "Axel", species = "puffin" }
-                    ]
+                    pets
                         |> Encode.encode
                             { encoder =
                                 Encode.withFieldNames
@@ -47,9 +49,7 @@ encodeTest =
                         |> Expect.equal "ID,Name,Species\u{000D}\n1,Atlas,cat\u{000D}\n2,Axel,puffin"
             , test "uses a blank character when a field name is missing" <|
                 \_ ->
-                    [ { id = 1, name = "Atlas", species = "cat" }
-                    , { id = 2, name = "Axel", species = "puffin" }
-                    ]
+                    pets
                         |> Encode.encode
                             { encoder =
                                 Encode.withFieldNames
@@ -62,5 +62,21 @@ encodeTest =
                             , fieldSeparator = ','
                             }
                         |> Expect.equal "ID,Name,Species\u{000D}\n1,,cat\u{000D}\n2,,puffin"
+            , test "uses the correct separator" <|
+                \_ ->
+                    pets
+                        |> Encode.encode
+                            { encoder =
+                                Encode.withFieldNames
+                                    [ "ID", "Name", "Species" ]
+                                    (\{ id, name, species } ->
+                                        [ ( "ID", String.fromInt id )
+                                        , ( "Name", name )
+                                        , ( "Species", species )
+                                        ]
+                                    )
+                            , fieldSeparator = ';'
+                            }
+                        |> Expect.equal "ID;Name;Species\u{000D}\n1;Atlas;cat\u{000D}\n2;Axel;puffin"
             ]
         ]
